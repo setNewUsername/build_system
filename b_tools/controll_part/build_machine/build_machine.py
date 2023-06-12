9ffb58ff3ea72029354e8615fddfc33a8b89f9ec
Alexandr Markin <sanakurvin@gmail.com>
2023-06-12 14:16:34 +0300

    reworked build machine control

--- b_tools/controll_part/build_machine/build_machine.py
+++ b_tools/controll_part/build_machine/build_machine.py
@@ -1,4 +1,4 @@
-import json, sys, os, argparse, requests, threading
+import json, sys, os, argparse, requests, threading, time
 sys.path.append('..\\..\\..\\')
 
 from b_tools.controll_part.common.command_reciever import command_reciever as CR
@@ -10,8 +10,6 @@ from purpose_handlers import *
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--coordinatorip', nargs='?', default='127.0.0.1')
-parser.add_argument('--coordinatorport', nargs='?', default='8000')
 parser.add_argument('--serverip', nargs='?', default='127.0.0.1')
 parser.add_argument('--serverport', nargs='?', default='8001')
 
@@ -25,11 +23,18 @@ class BuildMachine:
     coordinatorPort:int = None
     machineIsFree:bool = True
 
-    def __init__(self, machineAdr, machinePort, coordAdr, coordPort) -> None:
+    server:CR.CustomHTTPServer = None
+
+    def __init__(self, machineAdr, machinePort) -> None:
         self.machineAdress = machineAdr
         self.machinePort = int(machinePort)
-        self.coordinatorAdress = coordAdr
-        self.coordinatorPort = int(coordPort)
+
+    def setCoordinatorInfo(self, data):
+        self.coordinatorAdress = data['purpose_data']['coordinator_ip']
+        self.coordinatorPort = int(data['purpose_data']['coordinator_port'])
+        print(self.coordinatorAdress)
+        print(self.coordinatorPort)
+        return (201, 'new coordinator info setted')
 
     def sendRequestToCoordinator(self, message):
         return requests.post(f'http://{self.coordinatorAdress}:{self.coordinatorPort}', data=json.dumps(message)).json()
@@ -37,25 +42,38 @@ class BuildMachine:
     def stopBuild(self):
         pass
 
-    def registerMachine(self):
-        jsonData = self.sendRequestToCoordinator({
-            'purpose': 'register_machine',
-            'purpose_data':{
-                'build_host_ip': bMachine.machineAdress,
-                'build_host_port': bMachine.machinePort
-            }
-        })
-        print(jsonData)
-        self.machineId = jsonData['message_data']['machine_id']
-        print(f'Got response from coordinator; machine id: {self.machineId}')
+    def registerMachine(self, data):
+        if self.coordinatorAdress == None or self.coordinatorPort == None:
+            return (100, 'no coordinator data supplied')
+        try:
+            jsonData = self.sendRequestToCoordinator({
+                'purpose': 'register_machine',
+                'purpose_data':{
+                    'build_host_ip': bMachine.machineAdress,
+                    'build_host_port': bMachine.machinePort
+                }
+            })
+            print(jsonData)
+            self.machineId = jsonData['message_data']['machine_id']
+            print(f'Got response from coordinator; machine id: {self.machineId}')
+        except:
+            return (100, 'error while registering occured')
+        return (201, 'machine registrated successefully')
 
-    def unregisterMachine(self):
-        self.sendRequestToCoordinator({
-            'purpose': 'unregister_machine',
-            'purpose_data':{
-                'machine_id': f'{self.machineId}',
-            }
-        })
+    def unregisterMachine(self, data):
+        if self.machineId != None:
+            try:
+                self.sendRequestToCoordinator({
+                    'purpose': 'unregister_machine',
+                    'purpose_data':{
+                        'machine_id': f'{self.machineId}',
+                    }
+                })
+            except:
+                return (100, 'error while unregistering occured')
+            return (201, 'machine unregistered successefully')
+        else:
+            return (100, 'no machine id supplied; may be it wasn\'t registered')
 
     def startBuild(self, projectId):
         if self.machineIsFree:
@@ -91,20 +109,25 @@ class BuildMachine:
                     }
                 })
 
-bMachine = BuildMachine(namespace.serverip, namespace.serverport, namespace.coordinatorip, namespace.coordinatorport)
+bMachine = BuildMachine(namespace.serverip, namespace.serverport)
 
-bMachine.registerMachine()
+#bMachine.setCoordinatorInfo(namespace.coordinatorip, namespace.coordinatorport)
+
+#bMachine.registerMachine()
 
 def startServerFunc():
     print(f'Build machine server started at {bMachine.machineAdress}:{bMachine.machinePort}')
 
 def closeServerFunc():
-    bMachine.unregisterMachine()
+    bMachine.unregisterMachine(None)
     print('Build machine server stopped')
 
 startPHandler = StartBuildPurpose(['project_id'], bMachine.startBuild)
+setCoordinatorInfo = setCoordInfoPurpose(['coordinator_ip', 'coordinator_port'], bMachine.setCoordinatorInfo)
+registerMachine = regMachinePurposer(['placeholder'], bMachine.registerMachine)
+unregMachine = unregMachinePurpose(['placeholder'], bMachine.unregisterMachine)
 
-reqVal = CR.RequestValidator(['127.0.0.1'], purposeHandlers=[startPHandler])
+reqVal = CR.RequestValidator(None, purposeHandlers=[startPHandler, setCoordinatorInfo, registerMachine, unregMachine])
 
 resHad = CR.ResponseHandler(reqVal)
 
--- b_tools/controll_part/build_machine/purpose_handlers.py
+++ b_tools/controll_part/build_machine/purpose_handlers.py
@@ -1,14 +1,44 @@
-import sys, os
+import sys, os, threading
 sys.path.append('..\\..\\..\\')
 
 from b_tools.controll_part.common.command_reciever import command_reciever as CR
 
 class StartBuildPurpose(CR.BasePurposeHandler):
-    addProjectToQueueFunc = None
+    startBuildFunc = None
 
-    def __init__(self, reqFields: list, addPToQFunc) -> None:
+    def __init__(self, reqFields: list, startPFunc) -> None:
         super().__init__('start_build', reqFields, True)
-        self.addProjectToQueueFunc = addPToQFunc
+        self.startBuildFunc = startPFunc
 
-    def purposeProcess(self, jsonData):
-        return self.addProjectToQueueFunc(jsonData['purpose_data']['project_id'])
+    def purposeProcess(self, jsonData) -> tuple:
+        return self.startBuildFunc()
+    
+class setCoordInfoPurpose(CR.BasePurposeHandler):
+    setCoordInfoFunc = None
+
+    def __init__(self, reqFields: list, setInfoFunc) -> None:
+        super().__init__('set_coord_info', reqFields, True)
+        self.setCoordInfoFunc = setInfoFunc
+
+    def purposeProcess(self, jsonData) -> tuple:
+        return self.setCoordInfoFunc(jsonData)
+    
+class regMachinePurposer(CR.BasePurposeHandler):
+    regMachineFunc = None
+
+    def __init__(self, reqFields: list, regFunc) -> None:
+        super().__init__('reg_machine', reqFields, True)
+        self.regMachineFunc = regFunc
+
+    def purposeProcess(self, jsonData) -> tuple:
+        return self.regMachineFunc(jsonData)
+    
+class unregMachinePurpose(CR.BasePurposeHandler):
+    unregMachineFunc = None
+
+    def __init__(self, reqFields: list, unregFunc) -> None:
+        super().__init__('unreg_machine', reqFields, True)
+        self.unregMachineFunc = unregFunc
+
+    def purposeProcess(self, jsonData) -> tuple:
+        return self.unregMachineFunc(jsonData)
